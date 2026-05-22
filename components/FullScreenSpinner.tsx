46991bc2034a5e7e92efcb85c0485c8a0fd35ed8
Tamanna Rath <tamannaarath@gmail.com>
2026-05-22 11:34:25 +0530

    refactor: rename Spinner to FullScreenSpinner to resolve export name collision

--- components/FullScreenSpinner.tsx
+++ components/FullScreenSpinner.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export function Spinner() {
+export function FullScreenSpinner() {
   return (
     <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-black/60 backdrop-blur-md text-white">
       <div className="h-12 w-12 animate-spin rounded-full border-4 border-t-transparent border-white"></div>
