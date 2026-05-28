c0208005d4c11a51bf535aeaa48eb55a31d16d25
Angelina <angelinachatterjee2206@gmail.com>
2026-05-29 00:02:26 +0530

    chore: commit untracked copy-constants file

---
+++ src/lib/copy-constants.ts
@@ -0,0 +1,2 @@
+export const OFFLINE_DOUBT_QUEUED = "You are offline. Your doubt has been saved and will sync automatically when your connection returns.";
+export const OFFLINE_REPLY_QUEUED = "You are offline. Your reply has been saved and will sync automatically when your connection returns.";
